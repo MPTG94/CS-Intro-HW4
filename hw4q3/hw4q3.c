f8ebae9f5cf498bf6d452a4321dc940037c86d24
Mor Paz <1912.mor@gmail.com>
2019-06-02 01:30:13 +0300

    update answer for function f1 in q3

--- hw4q3/hw4q3.c
+++ hw4q3/hw4q3.c
@@ -8,7 +8,7 @@ int main()
     switch (question)
     {
     case 1:
-        printf("The answer to question 1 is t\n");
+        printf("The answer to question 1 is s\n");
         break;
     case 2:
         printf("The answer to question 2 is a\n");
@@ -20,7 +20,7 @@ int main()
         printf("The answer to question 4 is a\n");
         break;
     case 5:
-        printf("The answer to question 5 is z\n");
+        printf("The answer to question 5 is l\n");
         break;
     case 6:
         printf("The answer to question 6 is a\n");
